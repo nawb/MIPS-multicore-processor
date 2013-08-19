99ad4a08bf53650ce968b71cf5bb209b07759606
ECE 437L <ece437l@ecn.purdue.edu>
2013-08-18 23:41:01 -0400

    register file initial commit

---
+++ include/register_file_if.vh
@@ -0,0 +1,28 @@
+/*
+  Eric Villasenor
+  evillase@gmail.com
+
+  register file interface
+*/
+`ifndef REGISTER_FILE_IF_VH
+`define REGISTER_FILE_IF_VH
+
+// all types
+`include "cpu_types_pkg.vh"
+
+interface register_file_if;
+  // import types
+  import cpu_types_pkg::*;
+
+  logic     WEN;
+  regbits_t wsel, rsel1, rsel2;
+  word_t    wdat, rdat1, rdat2;
+
+  // register file ports
+  modport rf (
+    input   WEN, wsel, rsel1, rsel2, wdat,
+    output  rdat1, rdat2
+  );
+endinterface
+
+`endif //REGISTER_FILE_IF_VH
---
+++ testbench/register_file_tb.sv
@@ -0,0 +1,50 @@
+/*
+  Eric Villasenor
+  evillase@gmail.com
+
+  register file test bench
+*/
+
+// mapped needs this
+`include "register_file_if.vh"
+
+// mapped timing needs this. 1ns is too fast
+`timescale 1 ns / 1 ns
+
+module register_file_tb;
+
+  parameter PERIOD = 10;
+
+  logic CLK = 0, nRST = 0;
+
+  // test vars
+  int v1 = 1;
+  int v2 = 4721;
+  int v3 = 25119;
+
+  // clock
+  always #(PERIOD/2) CLK++;
+
+  // interface
+  register_file_if rfif ();
+  // DUT
+`ifndef MAPPED
+  register_file DUT(CLK, nRST, rfif);
+`else
+  register_file DUT(
+    .\rfif.rdat2 (rfif.rdat2),
+    .\rfif.rdat1 (rfif.rdat1),
+    .\rfif.wdat (rfif.wdat),
+    .\rfif.rsel2 (rfif.rsel2),
+    .\rfif.rsel1 (rfif.rsel1),
+    .\rfif.wsel (rfif.wsel),
+    .\rfif.WEN (rfif.WEN),
+    .\nRST (nRST),
+    .\CLK (CLK)
+  );
+`endif
+
+endmodule
+
+program test();
+endprogram
