a96431fdfd79a12381dc863a8c5aa10e2efacc1e
ECE 437L <ece437l@ecn.purdue.edu>
2013-08-21 00:19:56 -0400

    instruction saver added

--- source/caches.sv
+++ source/caches.sv
@@ -6,25 +6,49 @@
 */
 
 
+// interfaces
+`include "datapath_cache_if.vh"
+`include "cache_control_if.vh"
+
+// cpu types
+`include "cpu_types_pkg.vh"
+
 module caches (
   input logic CLK, nRST,
   datapath_cache_if.cache dcif,
   cache_control_if ccif
 );
+  // import types
+  import cpu_types_pkg::word_t;
+
   parameter CPUID = 0;
+
+  word_t instr;
 
   // icache
   //icache  ICACHE(dcif, ccif);
   // dcache
   //dcache  DCACHE(dcif, ccif);
 
+  // single cycle instr saver (for memory ops)
+  always_ff @(posedge CLK or negedge nRST)
+  begin
+    if (!nRST)
+    begin
+      instr <= '0;
+    end
+    else if (!ccif.iwait[CPUID] && (!dcif.dmemREN || !dcif.dmemWEN))
+    begin
+      instr <= ccif.iload[CPUID];
+    end
+  end
   // dcache invalidate before halt handled by dcache when exists
   assign dcif.flushed = dcif.halt;
 
-  //singlecycle
+  //single cycle
   assign dcif.ihit = (dcif.imemREN) ? ~ccif.iwait[CPUID] : 0;
   assign dcif.dhit = (dcif.dmemREN|dcif.dmemWEN) ? ~ccif.dwait[CPUID] : 0;
-  assign dcif.imemload = ccif.iload[CPUID];
+  assign dcif.imemload = instr;
   assign dcif.dmemload = ccif.dload[CPUID];
 
 
