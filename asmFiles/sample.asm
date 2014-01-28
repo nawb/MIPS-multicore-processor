f49eadb0ee4a4e6b7ec5f6dfb63457c1689b1338
Nabeel Zaim <nzaim@purdue.edu>
2014-01-28 17:24:45 -0500

    First commit for asm file #1

---
+++ asmFiles/sample.asm
@@ -0,0 +1,19 @@
+#---------------------------------------
+# sample asm file for tutoriala
+#---------------------------------------
+
+# set the address where you want this
+# code segment
+  org 0x0000
+
+  ori   $1, $0, mydata
+  ori   $2, $0, 0x0080
+  addu  $3, $1, $2
+  sw    $3, 4($1)
+  halt
+
+
+
+mydata:
+  org 0x0080
+  cfw   10
