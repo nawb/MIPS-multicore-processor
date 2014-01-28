18a3fc452d30a32d5060bee61e90ad37b62624cc
ECE 437L <ece437l@ecn.purdue.edu>
2014-01-27 23:49:44 -0500

    Changed ram and system.sv files to CPUCLK/RAMCLK

--- source/system.sv
+++ source/system.sv
@@ -9,17 +9,45 @@
 // system interface
 `include "system_if.vh"
 
+// types
+`include "cpu_types_pkg.vh"
+
 module system (input logic CLK, nRST, system_if.sys syif);
+
 
   // stopped running
   logic halt;
+
+  // clock division
+  parameter CLKDIV = 2;
+  logic CPUCLK;
+  logic [3:0] count;
+  //logic CPUnRST;
+
+  always_ff @(posedge CLK, negedge nRST)
+  begin
+    if (!nRST)
+    begin
+      count <= 0;
+      CPUCLK <= 0;
+    end
+    else if (count == CLKDIV-2)
+    begin
+      count <= 0;
+      CPUCLK <= ~CPUCLK;
+    end
+    else
+    begin
+      count <= count + 1;
+    end
+  end
 
   // interface
   cpu_ram_if                            prif ();
 
   // processor
-  //singlecycle #(.PC0('h0))              CPU (CLK, nRST, halt, prif);
-  pipeline    #(.PC0('h0))              CPU (CLK, nRST, halt, prif);
+  //singlecycle #(.PC0('h0))              CPU (CPUCLK, nRST, halt, prif);
+  pipeline    #(.PC0('h0))              CPU (CPUCLK, nRST, halt, prif);
   //multicore   #(.PC0('h0), .PC1('h200)) CPU (CLK, nRST, halt, prif);
 
   // memory
