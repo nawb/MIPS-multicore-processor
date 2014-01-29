b4fe5245cc1d0d90de4f70431f8d1892e1f58383
Nabeel Zaim <nzaim@purdue.edu>
2014-01-29 09:54:19 -0500

    Done with mult_procedure, stores to a wrong address but maybe I'm just reading memsim wrong

--- asmFiles/mult.asm
+++ asmFiles/mult.asm
@@ -4,36 +4,38 @@
 #--------------------------------------------
 
 	
-# set the address where you want this
-# code segment
-  org 0x0000
-
-mult:
-	#get 16 bits from stack (A)
-	#multiply it with each bit from (B)
-	#add them together
-	#store upper 16
-	#store lower 16
-
-#TEST VALUES:
+// set the address where you want this
+// code segment
+	org 0x0000
+	ori $29, $0, 0xFFFC   //initialize stack pointer to 0xFFFC
+	ori $31, $0, 0x3FFC   //where final result is stored
 	ori $1, $0, 0x0001
-	ori $2, $0, 0x0005
-	push $1
-	push $2
-#############
 
-	pop $5  #pop op2
-	pop $6  #pop op1
+	##############
+	#TEST VALUES:
+	ori $4, $0, 3
+	ori $5, $0, 2
+	push $4
+	push $5
+	##############
+mult:
+	pop $4  //pop op2
+	pop $5  //pop op1
+	and $2, $2, $0 //clear out $2
 
-#  ori   $1, $0, mydata
-#  ori   $2, $0, 0x0080
-#  addu  $3, $1, $2
-#  sw    $3, 4($1)
-  halt
-
-
-
+	//add op1 an op2 number of times
+	//store accumulating sum (/product) in $2
+mult_loop:
+	addu $2, $2, $5
+	subu $4, $4, $1  //decrement op2
+	beq $4, $0, mult_exit
+	j mult_loop
+mult_exit:
+	//push $2 //push return value from return register
+	sw $2, 0($31)
+	halt
+	
 mydata:
-	org 0x0080
-	cfw   10	
+	org 0x3FFC
+	cfw  0xAA	
 	
--- asmFiles/mult_procedure.asm
+++ asmFiles/mult_procedure.asm
@@ -4,33 +4,56 @@
 #--------------------------------------------
 
 	
-// set the address where you want this
-// code segment
+# set the address where you want this
+# code segment
 	org 0x0000
-	ori $29, $0, 0xFFFC   //initialize stack pointer to 0xFFFC
-	ori $31, $0, 0x3FFC   //where final result is stored
 	ori $1, $0, 0x0001
+	ori $29, $0, 0xFFFC   	//initialize stack pointer to 0xFFFC
 
+	##############
+	#TEST VALUES:
+	ori $24, $0, 2
+	ori $25, $0, 3
+	ori $26, $0, 6
+	##############
+	
+mult_procedure:
+	//INITIAL VALUES:
+	//load stack pointer value to finish at into $8:
+	andi $16, $0, 0
+	addiu $16, $29, -4
 
+	push $24
+	push $25
+	push $26
+
+mult_proc_loop:
+	beq $16, $29, mult_proc_loop_exit
+	jal mult
+	j mult_proc_loop
+	
+mult_proc_loop_exit:
+	addiu $31, $0, 0x3FFC
+	pop $8
+	sw $8, 0($31)
+	halt	
+
+#### MULTIPLY ALGORITHM
+#######################
 	
 mult:
-	pop $4  //pop op2
-	pop $5  //pop op1
-	and $2, $2, $0 //clear out $2
+	pop $8  	//pop op2
+	pop $9  	//pop op1
+	and $2, $2, $0 	//clear out $2
 
 	//add op1 an op2 number of times
 	//store accumulating sum (/product) in $2
 mult_loop:
-	addu $2, $2, $5
-	subu $4, $4, $1  //decrement op2
-	beq $4, $0, mult_exit
+	addu $2, $2, $9
+	subu $8, $8, $1 //decrement op2
+	beq $8, $0, mult_exit
 	j mult_loop
 mult_exit:
-	//push $2 //push return value from return register
-	sw $2, 0($31)
-	halt
-	
-mydata:
-	org 0x3FFC
-	cfw  0xAA	
-	
+	push $2		//push return value from return register
+	jr $31
+
