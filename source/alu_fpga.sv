4441f3b4cdc68a1a43eb06f85489e8a1373db913
Nabeel Zaim <nzaim@purdue.edu>
2014-01-28 19:03:53 -0500

    Fixing ALU wrapper

--- source/alu_fpga.sv
+++ source/alu_fpga.sv
@@ -10,35 +10,40 @@
 
 module alu_fpga 
   (
-   input logic 	      CLOCK_50,
-   input logic [3:0]  KEY,
-   input logic [17:0] SW,
-   output logic [6:0] HEX0;
-   output logic [6:0] HEX1;
-   output logic [6:0] HEX2;
-   output logic [6:0] HEX3;
+   input logic 	       CLOCK_50,
+   input logic [3:0]   KEY,
+   input logic [17:0]  SW,
+   output logic [6:0]  HEX0,
+   output logic [6:0]  HEX1,
+   output logic [6:0]  HEX2,
+   output logic [6:0]  HEX3,
+   output logic [6:0]  HEX4,
+   output logic [6:0]  HEX5,
+   output logic [6:0]  HEX6,
+   output logic [6:0]  HEX7,
+   output logic [16:0] LEDR,
+   output logic [3:0]  LEDG
    );
-
-   //internal registers
-   logic [15:0]       operand;   
    
    // interface
    alu_if aluif();
    // rf
-   alu ALU(CLOCK_50, KEY[2], aluif);
+   alu ALU(aluif);
    
-   assign aluif.op1 = SW[15:0];
-   assign aluif.op2 = {31'b0,SW[17]};
-   assign aluif.opcode = KEY[3:0];
+   assign aluif.op1  =  SW[17] & {{16{SW[16]}}, SW[15:0]};
+   assign aluif.op2  = ~SW[17] & {{16{SW[16]}}, SW[15:0]};
+   assign LEDR[15:0] =  SW[17] ? aluif.op1[15:0] : aluif.op2[15:0];
+
+   assign LEDG[0] = aluif.flag_z;
+   assign LEDG[1] = aluif.flag_n;
+   assign LEDG[2] = aluif.flag_v;
+   
+   
+   assign aluif.opcode = ~KEY[3:0];
    //assign HEX0[31:0] = aluif.res[31:0];
 
    always_comb
      begin
-	operand = SW[16] ? '1 : '0; //SW16 fills all bits
-
-	aluif.op1 =  SW[17] ? (operand | SW[15:0]) : '0;
-	aluif.op2 = ~SW[17] ? (operand | SW[15:0]) : '0;
-	
 	unique casez (aluif.res[3:0])
 	  'h0: HEX0 = 7'b1000000;
 	  'h1: HEX0 = 7'b1111001;
@@ -113,6 +118,82 @@ module alu_fpga
 	  'hd: HEX3 = 7'b0100001;
 	  'he: HEX3 = 7'b0000110;
 	  'hf: HEX3 = 7'b0001110;
+	endcase // unique casez (aluif.res[15:12])
+
+	unique casez (aluif.res[19:16])
+	  'h0: HEX4 = 7'b1000000;
+	  'h1: HEX4 = 7'b1111001;
+	  'h2: HEX4 = 7'b0100100;
+	  'h3: HEX4 = 7'b0110000;
+	  'h4: HEX4 = 7'b0011001;
+	  'h5: HEX4 = 7'b0010010;
+	  'h6: HEX4 = 7'b0000010;
+	  'h7: HEX4 = 7'b1111000;
+	  'h8: HEX4 = 7'b0000000;
+	  'h9: HEX4 = 7'b0010000;
+	  'ha: HEX4 = 7'b0001000;
+	  'hb: HEX4 = 7'b0000011;
+	  'hc: HEX4 = 7'b0100111;
+	  'hd: HEX4 = 7'b0100001;
+	  'he: HEX4 = 7'b0000110;
+	  'hf: HEX4 = 7'b0001110;
+	endcase // unique casez (aluif.res[15:12])
+	
+	unique casez (aluif.res[23:20])
+	  'h0: HEX5 = 7'b1000000;
+	  'h1: HEX5 = 7'b1111001;
+	  'h2: HEX5 = 7'b0100100;
+	  'h3: HEX5 = 7'b0110000;
+	  'h4: HEX5 = 7'b0011001;
+	  'h5: HEX5 = 7'b0010010;
+	  'h6: HEX5 = 7'b0000010;
+	  'h7: HEX5 = 7'b1111000;
+	  'h8: HEX5 = 7'b0000000;
+	  'h9: HEX5 = 7'b0010000;
+	  'ha: HEX5 = 7'b0001000;
+	  'hb: HEX5 = 7'b0000011;
+	  'hc: HEX5 = 7'b0100111;
+	  'hd: HEX5 = 7'b0100001;
+	  'he: HEX5 = 7'b0000110;
+	  'hf: HEX5 = 7'b0001110;
+	endcase // unique casez (aluif.res[15:12])
+	
+	unique casez (aluif.res[27:24])
+	  'h0: HEX6 = 7'b1000000;
+	  'h1: HEX6 = 7'b1111001;
+	  'h2: HEX6 = 7'b0100100;
+	  'h3: HEX6 = 7'b0110000;
+	  'h4: HEX6 = 7'b0011001;
+	  'h5: HEX6 = 7'b0010010;
+	  'h6: HEX6 = 7'b0000010;
+	  'h7: HEX6 = 7'b1111000;
+	  'h8: HEX6 = 7'b0000000;
+	  'h9: HEX6 = 7'b0010000;
+	  'ha: HEX6 = 7'b0001000;
+	  'hb: HEX6 = 7'b0000011;
+	  'hc: HEX6 = 7'b0100111;
+	  'hd: HEX6 = 7'b0100001;
+	  'he: HEX6 = 7'b0000110;
+	  'hf: HEX6 = 7'b0001110;
+	endcase // unique casez (aluif.res[15:12])
+	
+	unique casez (aluif.res[31:28])
+	  'h0: HEX7 = 7'b1000000;
+	  'h1: HEX7 = 7'b1111001;
+	  'h2: HEX7 = 7'b0100100;
+	  'h3: HEX7 = 7'b0110000;
+	  'h4: HEX7 = 7'b0011001;
+	  'h5: HEX7 = 7'b0010010;
+	  'h6: HEX7 = 7'b0000010;
+	  'h7: HEX7 = 7'b1111000;
+	  'h8: HEX7 = 7'b0000000;
+	  'h9: HEX7 = 7'b0010000;
+	  'ha: HEX7 = 7'b0001000;
+	  'hb: HEX7 = 7'b0000011;
+	  'hc: HEX7 = 7'b0100111;
+	  'hd: HEX7 = 7'b0100001;
+	  'he: HEX7 = 7'b0000110;
+	  'hf: HEX7 = 7'b0001110;
 	endcase
      end
 
