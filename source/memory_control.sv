714351fa7bb98f157260e52001baa775cda0f133
Nabeel Zaim <nzaim@purdue.edu>
2014-02-04 10:26:49 -0500

    Added always_comb to prioritize dEN

--- source/memory_control.sv
+++ source/memory_control.sv
@@ -1,10 +1,10 @@
 /*
-  Eric Villasenor
-  evillase@gmail.com
+ Eric Villasenor
+ evillase@gmail.com
 
-  this block is the coherence protocol
-  and artibtration for ram
-*/
+ this block is the coherence protocol
+ and arbitration for ram
+ */
 
 // interface include
 `include "cache_control_if.vh"
@@ -12,14 +12,26 @@
 // memory types
 `include "cpu_types_pkg.vh"
 
-module memory_control (
-  input CLK, nRST,
-  cache_control_if.cc ccif
-);
-  // type import
-  import cpu_types_pkg::*;
+module memory_control 
+  (
+   input CLK, nRST,
+   cache_control_if. cc ccif
+   );
+   // type import
+   import cpu_types_pkg::*;
 
-  // number of cpus for cc
-  parameter CPUS = 2;
+   // number of cpus for cc
+   parameter CPUS = 2;
+   
+   always_comb begin
+      ccif.dwait = 1'b0;
+      ccif.iwait = 1'b0;
+      if (ccif.dWEN|ccif.dREN) begin
+	 ccif.dwait = 1'b1;
+      end
+      else if (~(ccif.dWEN|ccif.dREN) & ccif.iREN) begin
+	 ccif.iwait = 1'b1;
+      end
+   end
 
 endmodule
