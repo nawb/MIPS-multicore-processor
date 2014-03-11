eef73d48b3d483544fb67d8755c0eb7f76297eb0
Nabeel Zaim <nzaim@purdue.edu>
2014-03-11 02:24:37 -0400

    Constructed dcache state machine from Steve's diagram.

--- source/dcache.sv
+++ source/dcache.sv
@@ -20,12 +20,16 @@ module dcache (
    import cpu_types_pkg::*;
    parameter CPUID = 0;
 
+   typedef enum  {IDLE, SELECT, WRITEBACK1, WRITEBACK2, FETCH1, FETCH2, HIT} states;
+   states cstate, nstate;   
+   
    typedef struct packed {
-      logic 	  valid;
-      logic [25:0] [2] tag;
-      word_t [2] data;
+      logic [25:0][2] tag;
+      word_t      [2] data;
+      logic 	      valid;
+      logic 	      dirty;
    } cache_block;
-
+   
    cache_block [8] cache;
    logic [25:0]    tag;
    logic [3:0] 	   index;
@@ -35,25 +39,87 @@ module dcache (
    assign index = dcif.dmemaddr[6:3];
    assign offset = dcif.dmemaddr[2];
 
-   always_ff @(posedge CLK, negedge nRST)
-     begin
-	if (!nRST)
-	  begin
-	     cache <= '0;
-	  end
-	else //if a cache miss:
+   always_ff @ (posedge CLK, negedge nRST) begin
+      if (!nRST) begin
+	 cstate <= IDLE;
+	 cache <= '0;	 
+      end
+      else
+	cstate <= nstate;
+   end
+   
+   always_comb begin : STATE_LOGIC
+      casez (cstate)	
+	IDLE: begin
+	   nstate = IDLE;
+	   if (dcif.dmemWEN|dcif.dmemREN)
+	     nstate = SELECT;
+	end
+	SELECT: begin
+	   nstate = SELECT;
+	   if (dcif.dhit) //hit
+	     nstate = HIT;
+	   else if (cache[index].dirty) //miss and dirty
+	     nstate = WRITEBACK1;
+	   else //miss but not dirty
+	     nstate = FETCH1;
+	end	
+	WRITEBACK1: begin
+	   if (dmemwait)
+	     nstate = WRITEBACK1;
+	   else
+	     nstate = WRITEBACK2;
+	end	
+	WRITEBACK2: begin
+	   nstate = FETCH1;	   
+	end	
+	FETCH1: begin
+	   if (dmemwait)
+	     nstate = FETCH1;
+	   else
+	     nstate = FETCH2;	   
+	end	
+	FETCH2: nstate = HIT;	   	
+	HIT: nstate = IDLE;	   	
+	default: nstate = IDLE;	    
+      endcase
+   end
+
+   always_comb begin : OUTPUT_LOGIC
+      casez(cstate)
+	IDLE: begin
+	   ccif.dWEN[CPUID] = 0;
+	   ccif.dREN[CPUID] = 0;	   
+	end
+	WRITEBACK1, WRITEBACK2: begin
+	   ccif.dWEN[CPUID] = 1;
+	   ccif.dREN[CPUID] = 0;	   
+	end
+	FETCH1, FETCH2: begin
+	   ccif.dWEN[CPUID] = 0;
+	   ccif.dREN[CPUID] = 1;
+	end
+	HIT: begin
+	   //nothing
+	end
+	default: begin
+	   ccif.dWEN[CPUID] = 0;
+	   ccif.dREN[CPUID] = 0;
+	end
+      endcase
+   end
+   
+/*
 	  if (!dcif.dhit && !ccif.dwait[CPUID]) begin
 	     cache[index].tag[offset] <= tag;
 	     cache[index].valid <= 1;
 	     cache[index].data[offset] <= ccif.dload[CPUID];
 	  end
-     end
+  */
    
-   assign dcif.dhit = (cache[index].tag == tag) && cache[index].valid;// && !ccif.iwait;
-   assign dcif.dmemload = cache[index].data;
+   assign dcif.dhit = (cache[index].tag[offset] == tag) && cache[index].valid;// && !ccif.iwait;
+   assign dcif.dmemload = cache[index].data[offset];
    
-   assign ccif.dREN[CPUID] = dcif.dmemREN && !dcif.dhit;
-   assign ccif.dWEN[CPUID] = dcif.dmemWEN && !dcif.dhit;   
    assign ccif.daddr[CPUID] = dcif.dmemaddr;
    
 endmodule
