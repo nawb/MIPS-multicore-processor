90611ac9becd1a21bcc095e8000ad6503e07107d
Nabeel Zaim <nzaim@purdue.edu>
2014-03-28 20:17:20 -0400

    Fixed pcEN logic. It would go down on a false halt. fib.asm does not blow out of the address range now. Forwarding issues remain.

--- source/datapath.sv
+++ source/datapath.sv
@@ -133,7 +133,7 @@ module datapath (
    assign pcif.imm16 = ppif.DE_out.pc_plus_4 + (ppif.DE_out.imm16 << 2);
    assign pcif.imm26 = ppif.DE_out.imm26;
    assign dpif.imemaddr = pcif.imemaddr;
-   assign pcif.pcEN = (~cuif.halt & dpif.ihit) & hzif.FDen;
+   assign pcif.pcEN = ((~cuif.halt & dpif.ihit) | hzif.branching | hzif.jumping) & hzif.FDen;
    //added OR branching so that PC doesn't shut down as soon as it sees a halt, because the halt
    //may have been a mispredict and we had actually meant to TAKE the branch 
    always_comb begin : BRANCHMUX
